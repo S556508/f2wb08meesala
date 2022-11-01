6549375d960c8fefac78a04fbb62a57914c8dc3f
S556508 <112007811+S556508@users.noreply.github.com>
2022-11-01 18:38:04 -0500

    added selector images

--- app.js
+++ app.js
@@ -7,6 +7,8 @@ var logger = require('morgan');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var woodRouter = require('./routes/wood');
+var gridRouter = require('./routes/gridbuild');
+var selectRouter = require('./routes/selector');
 var app = express();
 
 // view engine setup
@@ -22,6 +24,8 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/wood',woodRouter);
+app.use('/gridbuild',gridRouter);
+app.use('/selector',selectRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
